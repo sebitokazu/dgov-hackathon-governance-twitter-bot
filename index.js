f832db2610ba82f92a09fee79d7fb3eb9e86a03b
sebitokazu <sebitokazu@gmail.com>
2021-08-24 16:41:36 -0300

    Protocols info

--- index.js
+++ index.js
@@ -19,13 +19,32 @@ var listenForMentions = T.stream('statuses/filter', {track: `@${process.env.BOT_
 listenForMentions.on('tweet', mention);
 
 function mention(tweet){
-    const replyTo = tweet.in_reply_to_screen_name;
+    const replyTo = tweet.user.screen_name;
     const tweetId = tweet.id_str;
     const hashtags = tweet.entities.hashtags;
 
+
     if(hashtags.length != 0){
-        switch(hashtags[0].text){
+        switch(hashtags[0].text.toLowerCase()){
             case "protocols":
+                if(hashtags.length == 1){
+                    var data = BoardroomService.getProtocols();
+                    data.then((res)=>{
+                        res.data.forEach((protocol)=>{
+                            let text = createProtocolTweet(protocol);
+                            replyMention(tweetId, replyTo, text);
+                        });
+                    })
+                }else{
+                    let protocol = hashtags[1].text.toLowerCase();
+                    var data = BoardroomService.getProtocolsByName(protocol);
+                    data.then((res)=>{
+                        let text = createProtocolTweet(res.data);
+                        replyMention(tweetId, replyTo, text);
+                    }).catch((reason)=>{
+                        replyErrorMention(tweetId, replyTo);
+                    });
+                }
                 break;
             case "proposals":
                 break;
@@ -34,7 +53,7 @@ function mention(tweet){
             case "stats":
                 var data = BoardroomService.getStats();
                 data.then((stats)=>{
-                    var text = `\n Total Proposals: ${stats.data.totalProposals}`+
+                    let text = `\n Total Proposals: ${stats.data.totalProposals}`+
                     `\n Total Protocols: ${stats.data.totalProtocols}`+
                     `\n Total Unique Voters: ${stats.data.totalUniqueVoters}`+
                     `\n Total Votes Cast: ${stats.data.totalVotesCast} \n`
@@ -54,4 +73,22 @@ function replyMention(tweetId,replyTo, text){
         else
             console.log('Replied:' + reply.text);
     });
+}
+
+function replyErrorMention(tweetId,replyTo){
+    var replyText = `Sorry @${replyTo} , something is wrong with the request or maybe I screwed up... #bot`;
+    T.post('statuses/update', {status: replyText, in_reply_to_status_id: tweetId}, (err,reply)=>{
+        if(err)
+            console.log(err.message);
+        else
+            console.log('Replied:' + reply.text);
+    });
+}
+
+function createProtocolTweet(data){
+    return `\n Protocol: ${data.name}`+
+    `\n Symbol: ${data.tokens[0].symbol}`+
+    `\n Network: ${data.tokens[0].network}`+
+    `\n Price: ${data.tokens[0].marketPrices[0].price} ${data.tokens[0].marketPrices[0].currency}`+
+    `\n #${data.cname} \n`;
 }
